91967b86e4c57acd52593b279f2343f271c463b7
hoyeon-lyu <hoyeon-lyu.github.com>
2026-06-12 04:02:28 +0900

    setting load-0 added.

---
+++ main.py
@@ -0,0 +1,19 @@
+import GUI
+import tokeni
+import json
+
+with open("setting.json", "r+", encoding="utf-8") as f:
+    setting = json.load(f)
+
+class term_calc:
+    def setting(self):
+        pass
+    def start(self):
+        if setting["user_info"]["isdefault"] == True:
+            answer = input("Setting's now default. Change the setting?[Y/N]").lower()
+            if answer == "y":
+                setting()
+            elif answer == "n":
+                print("Skip the setting!")
+
+#I'm going to edit this things tomorrow. exam's next week...
