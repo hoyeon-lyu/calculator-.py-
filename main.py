170b3b2314982e2e035b3021939825b13eace320
hoyeon-lyu <hoyeon-lyu.github.com>
2026-06-12 04:15:49 +0900

    setting.json import with read-mode not a read&write-mode. and start() function print information at first.

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ import GUI
 import tokeni
 import json
 
-with open("setting.json", "r+", encoding="utf-8") as f:
+with open("setting.json", "r", encoding="utf-8") as f:
     setting = json.load(f)
 
 class term_calc:
     def setting(self):
         pass
     def start(self):
+        print("Hello! Your current information here!")
+        print(json.dumps(setting, indent = 4))
         if setting["user_info"]["isdefault"] == True:
             answer = input("Setting's now default. Change the setting?[Y/N]").lower()
             if answer == "y":
